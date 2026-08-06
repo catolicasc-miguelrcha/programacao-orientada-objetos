fc98d2eca1208f410a5ec1f31d0df1bf7599a1f3
miguelrcha <miguellrochaxavier@gmail.com>
2026-08-05 22:07:32 -0300

    feat: biblioteca mvc template

---
+++ Aula02 - Biblioteca MVC Arquitetura/Program.cs
@@ -0,0 +1,40 @@
+﻿using Biblioteca.model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Biblioteca
+{
+    // func principal para start app
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            BookModel book = new BookModel();
+
+            book.ISBN = "1";
+            book.title = "Fundamentos da arquitetura de software: uma abordagem de engenharia";
+            book.author = "Mark Richards, Neal Ford";
+            book.publisher = "O'REILLY";
+            book.genre = "IT";
+            book.borrowed = false;
+
+            book.ISBN = "2";
+            book.title = "Clean Code: A Handbook of Agile Software Craftsmanship";
+            book.author = "Robert C. Martin";
+            book.publisher = "Prentice Hall";
+            book.genre = "IT";
+            book.borrowed = false;
+
+
+            // Display book information, always (10, X) position
+            Console.SetCursorPosition(10, 5);
+            Console.WriteLine("Book: " + book.title);
+            
+
+
+        }
+    }
+}
---
+++ Aula02 - Biblioteca MVC Arquitetura/model/BookModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Biblioteca.model
+{
+    // Model class for Book
+    internal class BookModel
+    {
+        public String ISBN;
+        public String title;
+        public String author;
+        public String publisher;
+        public String genre;
+        public bool borrowed;
+
+
+        // Method
+        
+        public BookModel(String ISBN, String title, String author, String publisher, String genre, bool borrowed)
+        {
+            this.ISBN = ISBN;
+            this.title = title;
+            this.author = author;
+            this.publisher = publisher;
+            this.genre = genre;
+            this.borrowed = false;
+        }
+
+    }
+}
