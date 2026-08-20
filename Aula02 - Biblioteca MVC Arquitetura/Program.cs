9f3730f0ecd4f000d746ee167b619775a2133e0f
miguelrcha <miguellrochaxavier@gmail.com>
2026-08-19 21:59:56 -0300

    chore: enhance Screen class with additional methods for improved console management

--- Aula02 - Biblioteca MVC Arquitetura/Program.cs
+++ Aula02 - Biblioteca MVC Arquitetura/Program.cs
@@ -12,36 +12,50 @@ namespace Biblioteca
     {
         static void Main(string[] args)
         {
-            // BookModel book = new BookModel("1", "Fundamentos da arquitetura de software: uma abordagem de engenharia", "Mark Richards, Neal Ford", "O'REILLY", "IT", false);
-            // Display book information, always (10, X) position
-            // Console.SetCursorPosition(10, 5);
-            // Console.WriteLine("Book: " + book.title);
 
-            BookModel book = new BookModel();
+            Screen screen = new Screen(ConsoleColor.Green, ConsoleColor.Blue);
 
-            Console.Clear();
-            Console.WriteLine("-=-=-=-=-=-=     Add Book:   -=-=-=-=-=-=-");
-            Console.WriteLine("ISBN : ");
-            Console.WriteLine("Title : ");
-            Console.WriteLine("Author : ");
-            Console.WriteLine("Genre : ");
-            Console.WriteLine("Pages : ");
-            Console.WriteLine("-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=");
-                           
-            book.Isbn = Console.ReadLine();
-            book.Title = Console.ReadLine();
-            book.Author = Console.ReadLine();
-            book.Genre = Console.ReadLine();
-            book.Pages = int.Parse(Console.ReadLine());
-            Console.Clear();
+            screen.ScreenSetup();
+            screen.FrameAssembly(0, 0, 79, 24);
+            screen.CenterText("Sistema de Biblioteca", 1, 0, 79);
 
-            Console.WriteLine("Book added successfully!");
-            Console.WriteLine("Press the any key...");
+            screen.FrameAssembly(5, 3, 30, 10);
+            screen.CenterText("[1] Cadastrar Livro", 4, 5, 30);
+
+            screen.FrameAssembly(40, 10, 75, 23);
+            screen.CenterText("Lista de Livros", 11, 40, 75);
+
+            // popup
+            screen.FrameAssembly(40, 10, 75, 23);
+            screen.CenterText("Avisos importantes", 8, 20, 60);
+
             Console.ReadKey();
 
-            Console.Clear();
-            Console.WriteLine("-=-=-=-=-=-=  Book Information: -=-=-=-=-=-=");
-            Console.WriteLine("Book: " + book.Title);
+            // BookModel book = new BookModel();
+
+            // Console.Clear();
+            // Console.WriteLine("-=-=-=-=-=-=     Add Book:   -=-=-=-=-=-=-");
+            // Console.WriteLine("ISBN : ");
+            // Console.WriteLine("Title : ");
+            // Console.WriteLine("Author : ");
+            // Console.WriteLine("Genre : ");
+            // Console.WriteLine("Pages : ");
+            // Console.WriteLine("-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=");
+                           
+            // book.Isbn = Console.ReadLine();
+            // book.Title = Console.ReadLine();
+            // book.Author = Console.ReadLine();
+            // book.Genre = Console.ReadLine();
+            // book.Pages = int.Parse(Console.ReadLine());
+            // Console.Clear();
+
+            // Console.WriteLine("Book added successfully!");
+            // Console.WriteLine("Press the any key...");
+            // Console.ReadKey();
+
+            // Console.Clear();
+            // Console.WriteLine("-=-=-=-=-=-=  Book Information: -=-=-=-=-=-=");
+            // Console.WriteLine("Book: " + book.Title);
 
 
         }
--- Aula02 - Biblioteca MVC Arquitetura/view/Screen.cs
+++ Aula02 - Biblioteca MVC Arquitetura/view/Screen.cs
@@ -13,40 +13,92 @@ namespace Biblioteca.view
 
         public ConsoleColor TextColor { get => _textColor; set => _textColor = value; }
         public ConsoleColor BackgroundColor { get => _backgroundColor; set => _backgroundColor = value; }
-    
-    public Screen()
-    {
-            this.TextColor = ConsoleColor.Green;    
-            this.BackgroundColor = ConsoleColor.Black;
-    }
 
-    public Screen(ConsoleColor textColor, ConsoleColor backgroundColor)
-    {
+        public Screen()
+        {
+            this.TextColor = ConsoleColor.Green;
+            this.BackgroundColor = ConsoleColor.Black;
+        }
+
+        public Screen(ConsoleColor textColor, ConsoleColor backgroundColor)
+        {
             this.TextColor = text;
             this.BackgroundColor = background;
-    }
+        }
 
-    public void CenterText(string text, int line, int y, int x)
-    {
-        // power apps formmula 
-            int column = y + ((x-y-text.Length) / 2);
-            Conosole.SetCursorPosition(column, line);
+        public void CenterText(string text, int line, int y, int x)
+        {
+            // power apps formmula 
+            int column = y + ((x - y - text.Length) / 2);
+            Console.SetCursorPosition(column, line);
             Console.WriteLine(text);
-    }
+        }
 
-    public string Ask(string text, int line, int column)
-    {
-        string answer = "";
+        public string Ask(string text, int line, int column)
+        {
+            string answer = "";
 
-        Console.SetCursorPosition(column, line);
-        Console.Write(text);
-        answer = Console.ReadLine();
+            Console.SetCursorPosition(column, line);
+            Console.Write(text);
+            answer = Console.ReadLine();
 
-        return answer;
+            return answer;
 
-    }
+        }
+
+        public void FrameAssembly(int line, int column, int width, int height)
+        {
+            int Column, Line;
 
 
+            for (Column = column; Column < column + width; Column++)
+            {
+                Console.SetCursorPosition(Column, line);
+                Console.Write("="); // 205
+                Console.SetCursorPosition(Column, line + height);
+                Console.Write("="); // 205
+            }
+
+            for (Line = line; Line < line + height; Line++)
+            {
+                Console.SetCursorPosition(column, Line);
+                Console.Write("|"); // 186
+                Console.SetCursorPosition(column + width, Line);
+                Console.Write("|"); // 186
+            }
+
+
+            Console.SetCursorPosition(column, line);
+            Console.Write("+");
+            Console.SetCursorPosition(column, line);
+            Console.Write("+");
+            Console.SetCursorPosition(line, column);
+            Console.Write("+");
+            Console.SetCursorPosition(line, column);
+            Console.Write("+");
+        }
+
+        public void ScreenSetup()
+        {
+            Console.ForegroundColor = this.TextColor;
+            Console.BackgroundColor = this.BackgroundColor;
+            Console.Clear();
+
+
+        }
+
+
+        public void ClearScreen(int line, int column, int width, int height)
+        {
+            for (int l = line; l < line + height; l++)
+            {
+                for (int c = column; c < column + width; c++)
+                {
+                    Console.SetCursorPosition(c, l);
+                    Console.Write(" ");
+                }
+            }
+        }
 
     }
 }
